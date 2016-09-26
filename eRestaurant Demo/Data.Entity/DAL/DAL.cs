f0746481fb6d355729c56568b2e05a32250c26a4
wzhang28 <wzhang28@studentmail.nait.ca>
2016-09-26 15:55:42 -0600

    comment to DAL

--- eRestaurant Demo/Data.Entity/DAL/DAL.cs
+++ eRestaurant Demo/Data.Entity/DAL/DAL.cs
@@ -12,6 +12,10 @@ namespace Data.Entity
     // : DbContext means that my class ingerits from th ebContext class
     internal class RestaurantContext : DbContext
     {
+        // : base(string) is the constructor of the DbContext class that I call 
+        //                 before running the body of my own constructor.
+        // Hooking up constructors to call other constructors is called
+        // "Constructor Chaining"
         public RestaurantContext() : base("name=EatIn") { }
 
         public DbSet<SpecialEvent> SpecialEvents { get; set; }
