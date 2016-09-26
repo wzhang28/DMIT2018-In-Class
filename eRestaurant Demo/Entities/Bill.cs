cd76e89391e0eb88960b12ab784d34a9d229c99d
wzhang28 <wzhang28@studentmail.nait.ca>
2016-09-26 15:24:02 -0600

    Create entities

---
+++ eRestaurant Demo/Entities/Bill.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Bill
+    {
+        [Key]
+        public int BillID { get; set; }
+        public DateTime BillDate { get; set; }
+        public DateTime? OrderPlaced { get; set; }
+        public int NumberInParty { get; set; }
+        public bool PaidStatus { get; set; }
+        public int WaiterID { get; set; }
+        public int? TableID { get; set; }
+        public int? ReservationID { get; set; }
+        public bool OrderReady { get; set; }
+        public string Comment { get; set; }
+
+        // Navigation Properties
+        public virtual ICollection<BillItem> Items { get; set; }
+        public virtual Waiter Waiter { get; set; }
+        public virtual Table Table { get; set; }
+
+        public Bill()
+        {
+            BillDate = DateTime.Now;
+        }
+    }
+}
---
+++ eRestaurant Demo/Entities/MenuCategory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class MenuCategory
+    {
+        [Key]
+        public int MenuCategoryID { get; set; }
+        [Required(ErrorMessage = "A Description is required (5-35 characters)")]
+        [StringLength(35, MinimumLength = 5, ErrorMessage = "Descriptions must be from 5 to 35 characters in length")]
+        public string Description { get; set; }
+
+        public virtual ICollection<Item> MenuItems { get; set; }
+    }
+
+}
