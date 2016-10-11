1d2c8a0130c93fd3d79cc84ee41f945791c45b2d
wzhang28 <wzhang28@studentmail.nait.ca>
2016-10-11 17:15:45 -0600

    Security field 1

---
+++ Chinook Demo/Chinook.Framework/BLL/Scurity/UserManager.cs
@@ -0,0 +1,39 @@
+﻿using Chinook.Framework.DAL.Security;
+using Chinook.Framework.Entities.Security;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Linq;
+
+namespace Chinook.Framework.BLL.Scurity
+{
+    public class UserManager : UserManager<ApplicationUser>
+    {
+        public UserManager()
+            : base(new UserStore<ApplicationUser>(new ApplicationDbContext()))
+        {
+        }
+
+        #region Constants
+        private const string STR_DEFAULT_PASSWORD = "Pa$$word1";
+        /// <summary>Requires FirstName and LastName</summary>
+        private const string STR_USERNAME_FORMAT = "{0}.{1}";
+        /// <summary>Requires UserName</summary>
+        private const string STR_EMAIL_FORMAT = "{0}@ChinookLab.tha";
+        private const string STR_WEBMASTER_USERNAME = "Webmaster";
+        #endregion
+
+        public void AddWebMaster()
+        {
+            if (!Users.Any(u => u.UserName.Equals(STR_WEBMASTER_USERNAME)))
+            {
+                var webMasterAccount = new ApplicationUser()
+                {
+                    UserName = STR_WEBMASTER_USERNAME,
+                    Email = string.Format(STR_EMAIL_FORMAT, STR_WEBMASTER_USERNAME)
+                };
+                this.Create(webMasterAccount, STR_DEFAULT_PASSWORD);
+                this.AddToRole(webMasterAccount.Id, SecurityRoles.WebsiteAdmins);
+            }
+        } // End of AddWebMaster()
+    }
+}
